8147bfdea15362ab948e6079c9215bba0c75e64f
Hachi.TSAI <hachi.tsai@gmail.com>
2025-06-25 16:36:25 +0800

    kaggle practic day2

---
+++ 250624.py
@@ -0,0 +1,13 @@
+x = 14
+print(x)
+print(type(x))
+
+nearly_pi = 3.141592653589793238462643383279502884197169399375105820974944
+print(nearly_pi)
+print(type(nearly_pi))
+
+almost_pi = 3.14
+print(almost_pi)
+print(type(almost_pi))
+
+
