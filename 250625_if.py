6870784c31717a1a5d9c6c0b73e641a3ac8ca139
Hachi.TSAI <hachi.tsai@gmail.com>
2025-06-26 10:12:53 +0800

    250626

--- 250625_if.py
+++ 250625_if.py
@@ -2,7 +2,11 @@ def evaluate_temp(temp):
     message = "Normal temperature"
     if temp > 38:
         message = "Fever"
+    elif temp >= 32:
+        message = "Conforming to normal range"
+    else:
+        message = "Hypothermia"
     return message
 
-print(evaluate_temp(37.5))  # Output: Normal temperature
-print(evaluate_temp(39.0))  # Output: Fever
+print(evaluate_temp(33.8))  # Output: Normal temperature
+print(evaluate_temp(37))  # Output: Fever
