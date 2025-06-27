a5107f75a4bba6e29c8e6550c23c43e41639a761
Hachi.TSAI <hachi.tsai@gmail.com>
2025-06-27 08:17:15 +0800

    clasee list practice

--- 250627.py
+++ 250627.py
@@ -1,3 +1,11 @@
 #Kaggle_learn_Intro to Lists
 
 flowers_list = ["pink primrose", "hard-leaved pocket orchid", "canterbury bells", "sweet pea", "english marigold", "tiger lily", "moon orchid", "bird of paradise", "monkshood", "globe thistle"]
+print(flowers_list)
+print(type(flowers_list)
+      )
+print(len(flowers_list))  # Length of the list
+print(flowers_list[0])  # First element of the list
+print(flowers_list[1])  # Second element of the list
+print("Last_entry:", flowers_list[9])
+# Last element of the list
