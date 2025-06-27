a11f28ba159003628e6ced78db68b92f1899a867
Hachi.TSAI <hachi.tsai@gmail.com>
2025-06-27 08:25:38 +0800

    Class List practice2

--- 250627.py
+++ 250627.py
@@ -8,4 +8,15 @@ print(len(flowers_list))  # Length of the list
 print(flowers_list[0])  # First element of the list
 print(flowers_list[1])  # Second element of the list
 print("Last_entry:", flowers_list[9])
+print(flowers_list[-3])  # Last element of the list
 # Last element of the list
+
+hardcover_sales = [139, 128, 172, 139, 191, 168, 170]
+print(hardcover_sales)
+print(type(hardcover_sales))
+print("Entry at index 0:", hardcover_sales[0])  
+print("Entry at index 2:", hardcover_sales[2])  
+print("Minimum:",min(hardcover_sales))# Minimum value in the list
+print("Maximum:",max(hardcover_sales))# Maximum value in the list
+print("Sum:",sum(hardcover_sales))# Sum of all values in the list
+print("Average:", sum(hardcover_sales) / len(hardcover_sales))  # Average of the list
