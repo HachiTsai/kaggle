a8e12535de202f3f79b93a31f505d479fc4c54ab
Hachi.TSAI <hachi.tsai@gmail.com>
2025-06-27 08:14:04 +0800

    add cspell.json file

---
+++ 250627.py
@@ -0,0 +1,3 @@
+#Kaggle_learn_Intro to Lists
+
+flowers_list = ["pink primrose", "hard-leaved pocket orchid", "canterbury bells", "sweet pea", "english marigold", "tiger lily", "moon orchid", "bird of paradise", "monkshood", "globe thistle"]
